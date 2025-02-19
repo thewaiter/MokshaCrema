6fa7515532f70449dd43996cf0d910953193abe2
Robert Wiley <ylee@bodhilinux.com>
2025-02-19 09:15:54 -0500

    Use correct edc file name

--- build-legacy.sh
+++ build-legacy.sh
@@ -3,7 +3,7 @@
 
 printf "\n\033[1;33mWARNING:\033[0m For use with Bodhi Linux 7 only.\n\n"
 cd MokshaCrema
-sed -i 's/MokshaCrema/Icons-Moksha-Crema/g' default.edc
+sed -i 's/MokshaCrema/Icons-Moksha-Crema/g' crema.edc
 ./build.sh
-sed -i 's/Icons-Moksha-Crema/MokshaCrema/g' default.edc
+sed -i 's/Icons-Moksha-Crema/MokshaCrema/g' crema.edc
 cd ..
